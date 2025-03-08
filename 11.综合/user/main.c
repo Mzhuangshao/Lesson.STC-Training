15c622558035867424c7ca61ca78c3f75f3beb80
M_zhuangshao <79238849+Mzhuangshao@users.noreply.github.com>
2025-03-08 22:53:44 +0800

    update file

--- 11.综合/user/main.c
+++ 11.综合/user/main.c
@@ -5,36 +5,31 @@
 #include "LED.h"
 #include "KEY.h"
 
-void main(void)				//主函数
-{	
-	//推挽设置
+void main(void) // 主函数
+{
+	// 推挽设置
 	BitMap_Init();
 	Bleep_Init();
-	//定时器设置
+	// 定时器设置
 	Timer0Init();
-  Timer1Init();
-	while(1)
+	Timer1Init();
+	while (1)
 	{
-		if(trigger_1ms)				//1ms trigger
+		if (trigger_1ms) // 1ms trigger
 		{
 			trigger_1ms = 0;
-			KEY1_CHECK(); KEY2_CHECK();
+			KEY1_CHECK();
+			KEY2_CHECK();
 			BitMap_Display();
 			SysLED();
-			
 		}
-		if(trigger_10ms)			//10ms trigger
+		if (trigger_10ms) // 10ms trigger
 		{
 			trigger_10ms = 0;
-			
-			
+
 			Bleep();
 			main_LED();
 			main_BitMap();
-			
-			
-			
-			
 		}
 	}
 }
